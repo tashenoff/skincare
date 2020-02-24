d29f7b2cf0e1a3cfba3da18fc13f5826d9320ea6
tashenoff <kail.dead.kd@gmail.com>
2020-02-24 11:21:36 +0600

    modal

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,11 @@ $(function() {
 });
 
 var menu = document.getElementById('navbar');
+var close = document.getElementById('close');
+var open = document.getElementById('modal-open');
+var shadow = document.getElementById('example');
+
+// var body = document.body;
 
 window.onscroll = function() {
   scrollFunction();
@@ -19,4 +24,16 @@ function scrollFunction() {
     document.getElementById('navbar').classList.remove('nav--fixed');
     document.getElementById('space-navbar-fixed').removeAttribute('style');
   }
+}
+
+function openFunction() {
+  modal.classList.add('modal--is-visible');
+  document.body.classList.add('modal-open');
+  document.getElementById('bg_layer').style.display = 'block';
+}
+
+function closeFunction() {
+  modal.classList.remove('modal--is-visible');
+  document.body.classList.remove('modal-open');
+  document.getElementById('bg_layer').removeAttribute('style');
 }
